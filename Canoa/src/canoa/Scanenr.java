c264ff6505188c5c2d933a08b8c7334b96c763c0
Juliana Souza de Oliveira <JULIANASOU@cateno.com.br>
2019-09-05 14:39:23 -0300

    subindo

---
+++ Canoa/src/canoa/Scanenr.java
@@ -0,0 +1,67 @@
+package canoa;
+
+// <editor-fold defaultstate="collapsed" desc="imports...">
+import java.util.List;
+import java.util.ArrayList;
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="documentation...">
+/**
+ * Objective: ...
+ * 
+ * Description: ...
+ * 
+ * @version 1.0.0
+ * @author JULIANASOU, 5 de set de 2019, 09:07:31
+ */// </editor-fold>
+class Scanenr {
+    
+    //<editor-fold defaultstate="collapsed" desc="attributes...">
+    
+    //    <editor-fold defaultstate="collapsed" desc="main attributes...">
+    
+    //    </editor-fold>
+    
+    //    <editor-fold defaultstate="collapsed" desc="constants attributes...">
+    
+    //    </editor-fold>
+    
+    //    <editor-fold defaultstate="collapsed" desc="static attributes...">
+    
+    //    </editor-fold>
+    
+    //    <editor-fold defaultstate="collapsed" desc="auxiliary attributes...">
+    
+    //    </editor-fold>
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="constructors...">
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="methods...">
+    
+    //    <editor-fold defaultstate="collapsed" desc="getter and setter methods...">
+    
+    //    </editor-fold>
+    
+    //    <editor-fold defaultstate="collapsed" desc="override methods...">
+    
+    //    </editor-fold>    
+    
+    //    <editor-fold defaultstate="collapsed" desc="auxiliary methods...">
+    
+    //    </editor-fold>
+    
+    //    <editor-fold defaultstate="collapsed" desc="static methods...">
+    
+    //    </editor-fold>
+    
+    //    <editor-fold defaultstate="collapsed" desc="main methods...">
+    
+    //    </editor-fold>
+    
+    //</editor-fold>
+        
+}//class
